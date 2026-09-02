f735ea1d055246ff41660b6b21a693755711708a
Vosik <vosikvos@gmail.com>
2026-09-02 13:57:22 +0300

    Add Proxmox backup alert receiver

--- register_webhook.py
+++ register_webhook.py
@@ -1,7 +1,7 @@
 import os, httpx
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 bot = os.environ['TELEGRAM_BOT_TOKEN']; base = os.environ['PUBLIC_BASE_URL'].rstrip('/'); secret = os.environ['TELEGRAM_WEBHOOK_SECRET']
 r = httpx.post(f'https://api.telegram.org/bot{bot}/setWebhook', json={'url':f'{base}/telegram/webhook','secret_token':secret,'drop_pending_updates':True}, timeout=15)
